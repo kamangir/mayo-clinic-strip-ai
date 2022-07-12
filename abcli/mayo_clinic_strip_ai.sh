dc360f5a21bf47effe23dd2a11f76799abdae184
kamangir <arash@kamangir.net>
2022-07-12 12:02:14 -0400

    validation - kamangir/bolt#689

--- abcli/mayo_clinic_strip_ai.sh
+++ abcli/mayo_clinic_strip_ai.sh
@@ -8,8 +8,10 @@ function mayo_clinic_strip_ai() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_help_line "abct task_1" \
-            "run abct task_1."
+        abcli_help_line "mcsai install" \
+            "install mcsai."
+        abcli_help_line "mcsai validate" \
+            "validate mcsai."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m mayo_clinic_strip_ai --help
@@ -18,10 +20,13 @@ function mayo_clinic_strip_ai() {
         return
     fi
 
-    if [ "$task" == "task_1" ] ; then
-        python3 -m mayo_clinic_strip_ai \
-            task_1 \
-            ${@:2}
+    if [ "$task" == "install" ] ; then
+        conda install -c conda-forge openslide
+        return
+    fi
+
+    if [ "$task" == "validate" ] ; then
+        python3 -c "from openslide import OpenSlide"
         return
     fi
 
