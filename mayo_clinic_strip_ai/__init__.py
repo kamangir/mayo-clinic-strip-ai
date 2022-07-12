147de1755c93bc0fb7c2e83e06d71bd0bee0c843
kamangir <arash@kamangir.net>
2022-07-12 14:28:03 -0400

    openslide.read_region validated - kamangir/bolt#689

--- mayo_clinic_strip_ai/__init__.py
+++ mayo_clinic_strip_ai/__init__.py
@@ -1,5 +1,5 @@
 name = "mayo_clinic_strip_ai"
 
-version = "1.1.86"
+version = "1.1.88"
 
 description = "Mayo Clinic - STRIP AI"
---
+++ mayo_clinic_strip_ai/openslide.py
@@ -0,0 +1,45 @@
+from abcli import *
+from abcli import file
+from abcli import string
+from openslide import OpenSlide
+from pprint import pprint
+from abcli.logging import crash_report
+from abcli import logging
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def read_region(
+    filename,
+    region=(0, 0),
+    level=0,
+    size=(5000, 5000),
+    log_level=log_level,
+):
+    try:
+        slide = OpenSlide(filename)
+    except:
+        crash_report(f"-mcsai: openslide: read_region: {filename}.")
+        return False, None
+
+    logger.info(
+        "mcsai.openslide.read_region({}): {}, {} level(s), {} level dimension(s) - {}".format(
+            filename,
+            slide.dimensions,
+            slide.level_count,
+            slide.level_dimensions,
+            string.pretty_bytes(file.size(filename)),
+        )
+    )
+
+    if log_level > LOG_ON:
+        pprint(dict(slide.properties))
+
+    try:
+        image = slide.read_region(region, level, size)
+    except:
+        crash_report(f"-mcsai: openslide: read_region: {filename}.")
+        return False, None
+
+    return True, image
